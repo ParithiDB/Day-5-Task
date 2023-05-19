a680502691d95a33f24e421cc4474e351fd53ca8
ParithiDB <133184346+ParithiDB@users.noreply.github.com>
2023-05-19 15:04:15 +0530

    Create json_myResume.js

---
+++ json_myResume.js
@@ -0,0 +1,20 @@
+let myResume = {
+
+"_Name" : "XXX",
+"Age" : 24,
+"Education" : ["SSLC : 94%", "HSC : 82%", "UG (B.Tech(IT)) : 7.02 CGPA"],
+"Languages" : ["Tamil", "English", "Hindi"],
+"Skills" : ["C", "Javascript", "HTML", "Python", "MySql"],
+"Hobbies" : ["PUBG", "Cricket"],
+			 "contact" : {
+      "Phone no." : "9999999999",
+      "E-Mail id" : "abc@xmail.com",
+     	 "Address" : 
+      ["No. 69, Bharathiyar Street, Bhagat Singh Nagar, Dholakpur, India, Pin - 563785" ],
+      
+    }
+      
+}
+console.log(myResume);
+
+// Output Link: https://jsfiddle.net/q7w3yhj1/23/
